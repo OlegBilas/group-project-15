4c09be4578f3e3b31d7057ee3a3bc9d78d83e092
YevhenChaban <114156326+YevhenChaban@users.noreply.github.com>
2022-12-27 10:42:25 +0200

    adding js script, fixing modal issues

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,38 +1,8 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[ldata-modal-open]"),
-    closeModalBtn: document.querySelector("[ldata-modal-close]"),
-    modal: document.querySelector("[ldata-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[fdata-modal-open]"),
-    closeModalBtn: document.querySelector("[fdata-modal-close]"),
-    modal: document.querySelector("[fdata-modal]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
