ef5cbf9b65456c6adabaa9c64254969a489fb637
OlegBilas <114345352+OlegBilas@users.noreply.github.com>
2022-12-28 10:04:28 +0000

    Deploying to gh-pages from @ OlegBilas/group-project-15@0df16a870a4de55cbe1c070c50e0e03fbefd31c2 🚀

--- index.75007b4c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-open-ingri]"),closeModalBtn:document.querySelector("[data-close-ingri]"),modal:document.querySelector("[data-ingri]")};function n(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.75007b4c.js.map
