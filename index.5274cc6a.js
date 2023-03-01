d31f62a0623e26e0e7b1834168360a0ea8d488e7
OlegBilas <114345352+OlegBilas@users.noreply.github.com>
2023-03-01 11:48:26 +0000

    Deploying to gh-pages from @ OlegBilas/group-project-15@9ca7be1fc143d8f3de61b27c3dcced26b62a137e 🚀

--- index.5274cc6a.js
+++
@@ -1,2 +0,0 @@
-!function(){var d;d=document.querySelector(".js-overlay-modal"),window.matchMedia("(min-width: 768px)").addEventListener("change",(function(e){e.matches&&(d.classList.add("is-hidden"),bodyScrollLock.enableBodyScroll(document.body))}))}();
-//# sourceMappingURL=index.5274cc6a.js.map
---
+++ index.cfcc3842.js
@@ -0,0 +1,2 @@
+!function(){var d;d=document.querySelector(".js-overlay-modal:not(.is-hidden)"),window.matchMedia("(min-width: 768px)").addEventListener("change",(function(e){e.matches&&(d.classList.add("is-hidden"),bodyScrollLock.enableBodyScroll(document.body))}))}();
+//# sourceMappingURL=index.cfcc3842.js.map
